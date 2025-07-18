7b70498d017092edce945477be604a9670047ada
tanish0007 <jangratanish0007@gmail.com>
2025-07-18 13:14:58 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const FS = require('node:fs');
+// const test = require('node:test');
+const app = express();
+const PORT = 8080;
+
+
+app.use(express.json())
+app.use(express.static('public'));
+
+app.get('/testData', (req, res) => {
+    FS.readFile(__dirname+'/test.json', (err, data) => {
+        if(err){
+            return res.status(500).json({
+                "sucsess": false,
+                "error": "No Data"
+            })
+        }
+        const response = JSON.parse(data);
+        let testData = [];
+        let single ={};
+
+        for(let i=0; i<response.length; i++){
+            single.id = response[i].id;
+            single.que = response[i].que;
+            single.options = response[i].options
+
+            testData.push(single)
+        }
+        return res.status(200).json({
+            "sucess": true,
+            testData
+        })
+    })
+})
+
+app.post('/verify', (req, res) => {
+    const { queId, ans } = req.body;
+
+    if(!queId || !ans){
+        return res.status(400).json({
+            "sucsess": false,
+            "error": "Bad request from client"
+        })
+    }
+    FS.readFile(__dirname+'/answers.json', (err, data) => {
+        if(err){
+            return res.status(500).json({
+                "sucsess": false,
+                "error": "No Data"
+            })
+        }
+
+        const response = JSON.parse(data);
+        
+        let isCorrect = false;
+        for(let i=0; i<response.length; i++){
+            if(queId === response[i].id){
+                if(ans === response[i].ans){
+                    isCorrect = true;
+                }
+                else {
+                    isCorrect = false;
+                }
+            }
+        }
+
+        return res.status(200).json({
+            "success": true,
+            "isCorrect": isCorrect
+        })
+    })
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is runnning pn http://localhost:${PORT}`);
+})
