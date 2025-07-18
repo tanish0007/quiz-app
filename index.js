f3f838271690455b6d6ffe665cf7d074ca78c9e3
tanish0007 <jangratanish0007@gmail.com>
2025-07-18 15:53:20 +0530

    okay

--- index.js
+++ index.js
@@ -8,6 +8,33 @@ const PORT = 8080;
 app.use(express.json())
 app.use(express.static('public'));
 
+app.get('/question/:id', (req, res) => {
+    const id = req.params.id;
+
+    FS.readFile(__dirname+'/test.json', (err, data) => {
+        if(err){
+            return res.status(500).json({
+                "sucsess": false,
+                "error": "No Data"
+            })
+        }
+        const response = JSON.parse(data);
+        const question = response.find(elem => elem.id === id);
+
+        if(!question){
+            return res.status(404).json({
+            "sucess": false,
+            "error": "Not Found"
+        })
+        }
+        
+        return res.status(200).json({
+            "sucess": true,
+            question
+        })
+    })
+})
+
 app.get('/testData', (req, res) => {
     FS.readFile(__dirname+'/test.json', (err, data) => {
         if(err){
@@ -18,9 +45,9 @@ app.get('/testData', (req, res) => {
         }
         const response = JSON.parse(data);
         let testData = [];
-        let single ={};
 
         for(let i=0; i<response.length; i++){
+            let single ={};
             single.id = response[i].id;
             single.que = response[i].que;
             single.options = response[i].options
---
+++ public/js/test.js
@@ -0,0 +1,151 @@
+const user = JSON.parse(sessionStorage.getItem('user'));
+if(!user){
+    window.location.href = 'index.html';
+}
+
+const results = [];
+document.querySelector('.user-name').innerHTML = user.username;
+document.querySelector('.user-age').innerHTML = user.age;
+
+//------------------ fetch all questions -------------------
+async function fetchQuestions(){
+    try{
+        const res = await fetch(`http://localhost:8080/testData`);
+        const data = await res.json();
+        return data.testData;
+    } catch(err) {
+        console.log(err);
+    }
+}
+
+// ------------------ verify answers ------------------------
+async function verifyAns(quesId, answer){
+    try{
+        const res = await fetch('http://localhost:8080/verify',{
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                "queId": quesId,
+                "ans": answer
+            })
+        });
+        const data = await res.json();
+        return data.isCorrect;
+        console.log(data);
+    } catch(err) {
+        console.log(err);
+    }
+}
+const testContainer = document.querySelector('.test-container');
+const buttonBox = document.querySelector('.buttons');
+const que = document.querySelector('.que');
+
+// ---------- create side rado buttons ----------
+function createRadioButton(id) {
+    const radioInput = document.createElement('input');
+    radioInput.type = 'radio';
+    radioInput.id = `que${id}`;
+    radioInput.value = id;
+    radioInput.name = 'question';
+
+    const label = document.createElement('label');
+    label.htmlFor = `que${id}`;
+    label.textContent = id;
+
+    label.appendChild(radioInput);
+    buttonBox.appendChild(label);
+}
+
+function createQuestion(question){
+    const div = document.createElement('div');
+    div.classList.add('test-que');
+    div.setAttribute('id', `question-${question.id}`);
+    div.style.display = 'none';
+    div.innerHTML = `
+        <p id="${question.id}" class="que">${question.que}</p>
+        <div class="options">
+            <input type="radio" id="A-${question.id}" value=${question.options[0]} name="option"> <label for="A-${question.id}">${question.options[0]}</label>
+            <input type="radio" id="B-${question.id}" value=${question.options[1]} name="option"> <label for="B-${question.id}">${question.options[1]}</label>
+            <input type="radio" id="C-${question.id}" value=${question.options[2]} name="option"> <label for="C-${question.id}">${question.options[2]}</label>
+            <input type="radio" id="D-${question.id}" value=${question.options[3]} name="option"> <label for="D-${question.id}">${question.options[3]}</label>
+        </div>
+    `;
+
+    const button = document.createElement('button');
+    button.setAttribute('id', 'save-ans');
+    button.innerText = 'Save & Next';
+    button.addEventListener('click', async () => {
+        try{
+            const answers = div.querySelectorAll('input[name="option"]');
+            let answer = null;
+            // console.log(answers);
+            for(let i=0; i<answers.length; i++){
+                if(answers[i].checked){
+                    answer = answers[i].value;
+                }
+            }
+            if(answer === null){
+                console.log('Please select one');
+                return;
+            }
+            console.log(answer);
+            const result = {id: question.id, answer: answer};
+            results.push(result);
+            console.log(results);
+
+            const currentRadio = document.getElementById(`que${question.id}`);
+            const radios = [...document.querySelectorAll('input[name="question"]')];
+            const currentIndex = radios.findIndex(r => r === currentRadio);
+
+            if (currentIndex !== -1 && currentIndex < radios.length - 1) {
+                const nextRadio = radios[currentIndex + 1];
+                nextRadio.checked = true;
+
+                nextRadio.dispatchEvent(new Event('change'));
+            } else {
+                console.log("This is the last question.");
+            }
+            }
+        catch(err){
+            console.log(err);
+        }
+    })
+
+    div.appendChild(button);
+    testContainer.appendChild(div);
+}
+
+function showQuestions(questions) {
+    const radioButtons = document.querySelectorAll('input[name="question"]');
+    radioButtons.forEach(radio => {
+        radio.addEventListener('change', (e) => {
+            document.querySelectorAll('.test-que').forEach(que => {
+                que.style.display = 'none';
+            });
+            
+            const selectedId = e.target.value;
+            const questionDiv = document.getElementById(`question-${selectedId}`);
+            if (questionDiv) {
+                questionDiv.style.display = 'block';
+            }
+        });
+    });
+    
+    if (questions.length > 0) {
+        document.getElementById(`question-${questions[0].id}`).style.display = 'block';
+        document.getElementById(`que${questions[0].id}`).checked = true;
+    }
+}
+
+document.addEventListener('DOMContentLoaded', async() => {
+    try {
+        const response = await fetchQuestions();
+        response.forEach(element => {
+            createRadioButton(element.id);
+            createQuestion(element);
+        });
+        showQuestions(response);
+    } catch(err) {
+        console.log(err);
+    }
+});
